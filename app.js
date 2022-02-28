161e9a6fbe251f606e3b3df723b20dc01d98b580
andreablancovr@gmail.com <andreablancovr@gmail.com>
2022-02-28 14:34:32 +0100

    Creazione Repo

---
+++ app.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        // EsempioIndex: 0,
+        // Esempioarray: [
+        //     {
+        //         text: 'Fare X',
+        //         done: false,
+        //     },
+    },
+    methods: {
+        // Esempio: function ( i ) {
+        //     this.toDos.splice(i,1)
+        // },
+
+    },
+    // mounted() {
+    //     this.startAutoplay();
+    // },
+})
