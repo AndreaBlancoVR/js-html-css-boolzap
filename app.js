4d179278c4af7ffb6a34f50350c0805147da5e09
andreablancovr@gmail.com <andreablancovr@gmail.com>
2022-03-03 18:24:03 +0100

    Aggiunto qualche fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = new Vue({
     el: '#app',
     data: {
-        currentIndex: 0,
+        currentIndex: 1,
         search: '',
         searchIndex:  0,
         messageHoverIndex: false,
